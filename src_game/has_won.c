2077bc113bd5114747b6df080848ee54e8293bc3
Stuart Rapoport <stuartrapop@gmail.com>
2023-03-08 12:07:06 +0100

    updated sign in has won

--- src_game/has_won.c
+++ src_game/has_won.c
@@ -24,8 +24,8 @@ int has_won(t_size size, enum e_player tab[size.row][size.col], t_pos pos)
 	{
 		count_hor = update_counter(count_hor, token, size, tab, pos.y, pos.x + i);
 		count_ver = update_counter(count_ver, token, size, tab, pos.y + i, pos.x);
-		count_diag_up = update_counter(count_diag_up, token, size, tab, pos.y - i, pos.x + i);
-		count_diag_down = update_counter(count_diag_down, token, size, tab, pos.y + i, pos.x + i);
+		count_diag_up = update_counter(count_diag_up, token, size, tab, pos.y + i, pos.x + i);
+		count_diag_down = update_counter(count_diag_down, token, size, tab, pos.y - i, pos.x + i);
 		if (count_hor >= 4 || count_ver >= 4 || count_diag_up >= 4 || count_diag_down >= 4)
 			return (1);
 		i++;
